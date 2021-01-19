0b4d8bb96deebc8f19b976e3762e435479865f2a
chinh7 <chinh7@me.com>
2021-01-19 17:55:59 +0700

    Upgrade WABT version for travis build (#30)
    * upgrade WABT version for travis build

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -6,7 +6,7 @@ wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/ins
 sudo mv ./bin/* /usr/local/bin/
 
 # Download and extract wabt
-wget -c https://github.com/WebAssembly/wabt/releases/download/1.0.12/wabt-1.0.12-linux.tar.gz -O - | tar -xz && ls
+wget -c https://github.com/WebAssembly/wabt/releases/download/1.0.20/wabt-1.0.20-ubuntu.tar.gz -O - | tar -xz && ls
 
 # Move wat2wasm to path
-sudo mv ./wabt-1.0.12/* /usr/local/bin/
+sudo mv ./wabt-1.0.20/bin/* /usr/local/bin/
